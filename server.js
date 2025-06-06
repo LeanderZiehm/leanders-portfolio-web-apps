f95cd673013203a85288af849f980e3d2e68f48a
Leander <leander-arun.ziehm@stud.th-deg.de>
2025-06-06 17:33:46 +0200

    require

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import session from 'express-session';
-import dotenv from 'dotenv';
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
 
 dotenv.config();
 
